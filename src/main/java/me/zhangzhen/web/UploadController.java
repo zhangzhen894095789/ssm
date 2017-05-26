751cb4468ce536d4ed16efd92ac62339700cc33b
zhangzhen <zzwwy@163.com>
2017-05-26 18:27:04 +0800

    update

---
+++ src/main/java/me/zhangzhen/web/UploadController.java
@@ -0,0 +1,25 @@
+package me.zhangzhen.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequestMapping(value = "/upload")
+public class UploadController {
+    
+    @RequestMapping(value = "/uploadPic.do")
+    @ResponseBody
+    public String uploadPic(@RequestParam(required=false) MultipartFile pic){
+        System.out.println(pic.getOriginalFilename());
+        return "success";
+    }
+    
+    @RequestMapping(value = "/test.do")
+    @ResponseBody
+    public String test(){
+    	return "哈哈哈";
+    }
+}
