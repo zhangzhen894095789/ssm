65abff8270a2f9edbc4cae58fbc13f652daf0166
zhangzhen <zzwwy@163.com>
2017-06-07 20:22:35 +0800

    update

---
+++ src/main/java/me/zhangzhen/filter/MyFilter.java
@@ -0,0 +1,37 @@
+package me.zhangzhen.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyFilter implements Filter {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+			throws IOException, ServletException {
+//		System.out.println("json 过滤器");
+//		HttpServletResponse response = (HttpServletResponse) arg1;
+//		response.setHeader("Content-Type", "application/json");
+//		arg2.doFilter(arg0, response);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
