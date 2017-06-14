5dbbe860ac664cdaebd37956044d83b8972df7b2
zhangzhen <zzwwy@163.com>
2017-06-14 21:00:52 +0800

    update swagger

--- src/main/java/me/zhangzhen/swagger/SwController.java
+++ src/main/java/me/zhangzhen/swagger/SwController.java
@@ -1,22 +1,19 @@
 package me.zhangzhen.swagger;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import me.zhangzhen.common.JsonResult;
-import net.sf.json.JSONObject;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
+import me.zhangzhen.domian.User;
 
 @Api(value = "api")
 @Controller
@@ -33,9 +30,13 @@ public class SwController {
 	@RequestMapping(value = "/test", method = RequestMethod.POST)
 	@ResponseBody
 	@ApiOperation(value = "测试方法", httpMethod = "POST")
-	public JsonResult testIgnoreAuth(HttpServletRequest req,@RequestParam String test) {
+	public JsonResult testIgnoreAuth(HttpServletRequest req,@RequestParam String test,@RequestBody User user) {
+		user.setId(2);
+		user.setUserName("test");
+		user.setPassword("test");
+		user.setGender("test");
 		JsonResult result = new JsonResult();
 		String _token = req.getParameter("test");
-		return result.ok().put("test", _token);
+		return result.ok().put("test", _token).put("user", user);
 	}
 }
